23a28819ab3308e016ad6337a13947d8ec795224
karmele <karmeledai@hotmail.com>
2019-11-18 09:31:01 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+include "vistas/cabecera.php";
+echo "<h4>Lista de viñetas con for</h4>";
+echo "<ul>";
+for ($i = 1; $i <= 10; $i++) {
+
+    echo "<li>Iteración " . $i . " </li>";
+}
+echo "</ul>";
+$z = 1;
+
+echo "<h4>Lista de viñetas con While</h4>";
+echo "<ol>";
+while ($z <= 10) {
+
+    echo "<li>Iteración " . $z . " </li>";
+
+    $z++;
+}
+echo "<ol>";
+include "vistas/pie.php";
